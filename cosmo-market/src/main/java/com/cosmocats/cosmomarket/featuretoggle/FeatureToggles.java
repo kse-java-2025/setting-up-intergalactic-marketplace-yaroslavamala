12a034e7b51b186618d034c1f91f6b0941d32c34
Yaroslava Mala <ymala@kse.org.ua>
2025-11-19 18:35:42 +0200

    implemented lab-2 task

---
+++ cosmo-market/src/main/java/com/cosmocats/cosmomarket/featuretoggle/FeatureToggles.java
@@ -0,0 +1,17 @@
+package com.cosmocats.cosmomarket.featuretoggle;
+
+import lombok.Getter;
+
+@Getter
+public enum FeatureToggles {
+
+    COSMO_CATS("cosmoCats"),
+    KITTY_PRODUCTS("kittyProducts");
+
+    private final String featureName;
+
+    FeatureToggles(String featureName) {
+        this.featureName = featureName;
+    }
+
+}
